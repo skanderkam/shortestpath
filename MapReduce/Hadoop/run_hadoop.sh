3de12c94297afd652ef922a279d1513e308337e9
Skander Kamoun <44209351+skanderkam@users.noreply.github.com>
2019-03-01 10:12:17 +0100

    Running Tests

--- MapReduce/Hadoop/run_hadoop.sh
+++ MapReduce/Hadoop/run_hadoop.sh
@@ -17,10 +17,10 @@ launch_job() {
   hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
   -input /user/hadoop/wc/input \
   -output /user/hadoop/wc/output \
-  -file /home/hadoop/mapper_test.py \
-  -mapper /home/hadoop/mapper_test.py \
-  -file /home/hadoop/reducer_test.py \
-  -reducer /home/hadoop/reducer_test.py  
+  -file /home/hadoop/mapper.py \
+  -mapper /home/hadoop/mapper.py \
+  -file /home/hadoop/reducer.py \
+  -reducer /home/hadoop/reducer.py  
 }
 
 init
