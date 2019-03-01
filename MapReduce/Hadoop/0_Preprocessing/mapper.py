22a8c360eceb2c77b55d1fb5f1b8e9938e09a078
Skander Kamoun <44209351+skanderkam@users.noreply.github.com>
2019-03-01 09:41:40 +0100

    Running Tests

--- MapReduce/Hadoop/0_Preprocessing/mapper.py
+++ MapReduce/Hadoop/0_Preprocessing/mapper.py
@@ -1,9 +1,6 @@
+
 #!/usr/bin/python
 import sys
-
-# Initializing the start variable
-start_file = open('MapReduce/Local/tmp/_start.txt', 'r')
-start = start_file.readline(1)
 
 # Initialization Map
 for line in sys.stdin:
@@ -15,6 +12,4 @@ for line in sys.stdin:
     dist = float('inf')
 
     # Distance = 0 if the node is the start
-    if line[0] == start:
-        dist = 0
     print '%s\t(%s,(%s;%s),%s)' % (line[0], dist, line[2], line[1], " ")
