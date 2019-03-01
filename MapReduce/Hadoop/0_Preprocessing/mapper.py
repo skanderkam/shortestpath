db5b033dbbff457bb343e532b9be0e36843bce5a
Skander Kamoun <44209351+skanderkam@users.noreply.github.com>
2019-03-01 10:27:18 +0100

    Running Tests

--- MapReduce/Hadoop/0_Preprocessing/mapper.py
+++ MapReduce/Hadoop/0_Preprocessing/mapper.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 import sys
+import os
 
-# Initializing the start variable
-start_file = open('/home/hadoop/tmp/_start.txt', 'r')
-start = start_file.readline(1)
+import os
+cwd = os.getcwd()
 
-# Initialization Map
 for line in sys.stdin:
-    # Parse the input from the graph
-    line = line.strip('\n')
-    line = line.split('\t')
-    
-    # Distance = infinity by default
-    dist = float('inf')
+    print(cwd)
 
-    # Distance = 0 if the node is the start
-    if line[0] == start:
-        dist = 0
-    print '%s\t(%s,(%s;%s),%s)' % (line[0], dist, line[2], line[1], " ")
+
+
+# # Initializing the start variable
+# start_file = open('/home/hadoop/tmp/_start.txt', 'r')
+# start = start_file.readline(1)
+
+# # Initialization Map
+# for line in sys.stdin:
+#     # Parse the input from the graph
+#     line = line.strip('\n')
+#     line = line.split('\t')
+    
+#     # Distance = infinity by default
+#     dist = float('inf')
+
+#     # Distance = 0 if the node is the start
+#     if line[0] == start:
+#         dist = 0
+#     print '%s\t(%s,(%s;%s),%s)' % (line[0], dist, line[2], line[1], " ")
+
+# with client_hdfs
--- MapReduce/Hadoop/0_Preprocessing/reducer.py
+++ MapReduce/Hadoop/0_Preprocessing/reducer.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python
 import sys
 
-# Initializing the variable we are going to loop on
-current_node = None
-current_dist = None
-current_path = None
-neighbors = []
-
-# input comes from STDIN
 for line in sys.stdin:
-    # Parse the input from mapper.py
-    line = line.strip('\n')
-    key, value = line.split('\t')
-    value_clean = value[1:-1].split(',')
+    print(line)
 
-    # Giving new names for more clarity
-    node = key
-    neighbor = value_clean[1]
-    dist = value_clean[0]
-    path = value_clean[2]
+# # Initializing the variable we are going to loop on
+# current_node = None
+# current_dist = None
+# current_path = None
+# neighbors = []
 
-    # Regrouping the neighbors for similar keys (since they are sorted by Hadoop but not grouped)
+# # input comes from STDIN
+# for line in sys.stdin:
+#     # Parse the input from mapper.py
+#     line = line.strip('\n')
+#     key, value = line.split('\t')
+#     value_clean = value[1:-1].split(',')
 
-    # Append the neighbors if key is similar
-    if current_node == node:
-        neighbors.append(neighbor)
-    # Printing what we have so far if the key is different, and then moving to the next node if key is different
-    else:
-        if current_node:
-            # Write result to STDOUT
-            print '%s\t(%s/%s/%s)' % (current_node, current_dist, neighbors, current_path)
-        # Moving to the next node
-        current_node = node
-        current_dist = dist
-        current_path = path
-        neighbors = [neighbor]
+#     # Giving new names for more clarity
+#     node = key
+#     neighbor = value_clean[1]
+#     dist = value_clean[0]
+#     path = value_clean[2]
 
-# We do not forget to print the last element not printed by the for loop
-if current_node == node:
-    print '%s\t(%s/%s/%s)' % (current_node, current_dist, neighbors, current_path)
+#     # Regrouping the neighbors for similar keys (since they are sorted by Hadoop but not grouped)
+
+#     # Append the neighbors if key is similar
+#     if current_node == node:
+#         neighbors.append(neighbor)
+#     # Printing what we have so far if the key is different, and then moving to the next node if key is different
+#     else:
+#         if current_node:
+#             # Write result to STDOUT
+#             print '%s\t(%s/%s/%s)' % (current_node, current_dist, neighbors, current_path)
+#         # Moving to the next node
+#         current_node = node
+#         current_dist = dist
+#         current_path = path
+#         neighbors = [neighbor]
+
+# # We do not forget to print the last element not printed by the for loop
+# if current_node == node:
+#     print '%s\t(%s/%s/%s)' % (current_node, current_dist, neighbors, current_path)
